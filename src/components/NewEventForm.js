78ca9238318c771e145b547e6afe4fbcafffc99b
JenishbekJanybaev <dj.jenish82@gmail.com>
2025-04-04 11:16:36 +0600

    Event Form2

--- src/components/NewEventForm.js
+++ src/components/NewEventForm.js
@@ -1,19 +1,29 @@
 
 import './NewEventForm.css'
+import { useState } from 'react'
 
 const NewEventForm = () => {
+    const [title, setTitle] = useState('')
+    const [date, setDate] = useState('')
+    
     return (
         <form className='new-event-form'>
             <label>
                 <span>Event Title:</span>
-                <input type="text" />
+                <input type="text" onChange={(e) => setTitle(e.target.value)}/>
             </label>
             <label>
                 <span>Event Date:</span>
-                <input type="date" />
+                <input type="date" onChange={(e) => setDate(e.target.value)}/>
             </label>
-
+            <button type='button'>reset inputs</button>
+            <br />
+            <br />
             <button>Submit</button>
+            <hr />
+            <p>Title:{ title}</p>
+            <p>Date:{ date}</p>
+
         </form>
     )
 }
